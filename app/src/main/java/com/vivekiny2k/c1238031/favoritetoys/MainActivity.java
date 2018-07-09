63162b513c90e68987514bd6cdc297604671e52f
Vivek Yadav <vivekiny2k@gmail.com>
2018-07-09 14:25:05 -0400

    test1

---
+++ app/src/main/java/com/vivekiny2k/c1238031/favoritetoys/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vivekiny2k.c1238031.favoritetoys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
